568823ae382975e980d0c7a7ac7a25a7f8794928
nannabeacm <anna.bcmarques@gmail.com>
2026-01-28 14:47:27 +0000

    part um dos exercicios

---
+++ exercise-bundle/part_one.py
@@ -0,0 +1,117 @@
+### 01 THE FIRST WORDS 
+print("\n===== 01 ======\n")
+
+print("I am Iron Man.")
+
+
+### 02 IDENTITY REVEAL
+print("\n===== 02 ======\n")
+
+hero = "Batman"
+
+print(hero)
+
+### 03
+print("\n===== 03 ======\n")
+
+part_one = "Avengers"
+part_two = "Assemble"
+
+print(f"{part_one} {part_two}")
+
+### 04
+print("\n===== 04 ======\n")
+
+strength_point_one = 100
+strength_point_two = 55
+
+result = strength_point_one + strength_point_two
+
+print(result)
+
+### 05
+print("\n===== 05 ======\n")
+
+miles = 500
+hours = 2.5
+
+average_speed = miles / hours
+
+print(average_speed)
+
+
+### 06
+print("\n===== 06 ======\n")
+
+is_invincible = False
+
+print(is_invincible)
+
+
+### 07
+print("\n===== 07 ======\n")
+
+name = input("What's your super-hero name? ")
+
+print(f"Welcome, {name}.")
+
+# # ### 08
+print("\n===== 08 ======\n")
+
+age_ultron = int(input("What's Ultron's age? "))
+
+print(age_ultron + 1)
+
+### 09
+print("\n===== 09 ======\n")
+
+color = "Red"
+material = "Gold"
+
+print(f"The suit is {color} and {material}.")
+
+### 10
+print("\n===== 10 ======\n")
+
+squashing_hulk = "hulk squash"
+
+print(squashing_hulk.upper())
+
+### 11
+print("\n===== 11 ======\n")
+
+true_supes_name = "Clark Kent"
+
+print(true_supes_name.replace("Clark","Superman"))
+
+### 12
+print("\n===== 12 ======\n")
+
+power = 9000
+
+print(type(power))
+
+### 13
+print("\n===== 13 ======\n")
+
+heroes = 7
+teams = 3
+
+heroes_left = heroes % teams
+print(heroes_left)
+
+### 14
+print("\n===== 14 ======\n")
+
+power_scaling = 2**10
+
+print(power_scaling)
+
+### 15
+print("\n===== 15 ======\n")
+
+total_damage = (50 * 2) + (100 / 2) - 25
+
+print(total_damage)
+
+
