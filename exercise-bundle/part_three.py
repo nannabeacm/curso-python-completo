be0cc4056532ae1affbb50540dc629b164ea8d8f
nannabeacm <anna.bcmarques@gmail.com>
2026-01-29 16:29:55 +0000

    parte dois e três dos exercicios

---
+++ exercise-bundle/part_three.py
@@ -0,0 +1,156 @@
+### 31
+print("\n===== 31 ======\n")
+
+number_start = 1
+
+while number_start <= 5:
+    print(number_start)
+    number_start += 1
+
+### 32
+print("\n===== 32 ======\n")
+
+heroes = ["Batman", "Superman", "Wonder Woman"]
+
+for item in heroes:
+   print(item)
+
+### 33
+print("\n===== 33 ======\n")
+
+countdown = 3
+
+while countdown > 0:
+    print(countdown)
+    countdown -= 1
+
+print("Liftoff!") 
+
+### 34
+print("\n===== 34 ======\n")
+
+hits = [10, 20, 15]
+total_damage = 0
+
+for item in hits:
+    total_damage += item
+
+print(total_damage)
+
+### 35
+print("\n===== 35 =====\n")
+
+people = ["Hero", "Hero", "Spy", "Hero"]
+
+for item in people:
+    if "Spy" in people:
+        print("Found him!")
+    break
+
+### 36
+print("\n===== 36 ======\n")
+
+number_count = 0
+
+while number_count < 5:
+    number_count += 1
+    if number_count == 3:
+        continue
+    print(number_count)
+
+### 37
+print("\n===== 37 ======\n")
+
+for i in range(0, 21, 5):
+    print(i)
+
+### 38
+print("\n===== 38 ======\n")
+
+for x in range(2):
+    for y in range(2):
+        print(f"({x}, {y})")
+
+### 39
+print("\n===== 39 ======\n")
+
+secret = 7
+guess = None
+
+while guess != secret:
+    guess = int(input("Guess: "))
+    if guess == secret:
+        print("Correct")
+
+### 40
+print("\n===== 40 ======\n")
+
+racers = ["Flash", "Quicksilver"]
+
+for position, name in enumerate(racers):
+    print(f"{position +1}.", name)
+
+### 41
+print("\n===== 41 ======\n")
+
+code = "S.H.I.E.L.D."
+
+for char in code:
+    if char != ".":
+        print(char, end="")
+
+### 42
+print("\n===== 42 ======\n")
+
+reps = 0
+
+while reps < 3:
+    print("Lifting...")
+    reps += 1
+
+### 43
+print("\n===== 43 ======\n")
+
+for i in range(1, 4):
+    print(i, i**2)
+
+### 44
+print("\n===== 44 ======\n")
+
+char_list = ["Stark", "Thanos", "Rogers"]
+found = None
+
+for item in char_list:
+    if "Thanos" in char_list:
+        found = True
+        if found:
+            print("Thanos found")
+    break
+
+
+# 45. Infinity Stones
+# Difficulty: Hard Mission: 
+# Count how many times "Stone" appears in ["Stone", "Rock", "Stone", "Gem"] using a loop (not .count()).
+#  Example Output:
+
+# 2
+
+### 45
+print("\n===== 45 ======\n")
+
+infinity_stones = ["Stone", "Rock", "Stone", "Gem"]
+
+
+
+
+### 45
+print("\n===== 45 ======\n")
+
+infinity_stones = ["Stone", "Rock", "Stone", "Gem"]
+stone_count = 0
+
+for stone in infinity_stones:
+    if stone == "Stone":
+        stone_count += 1
+
+print(stone_count)
---
+++ exercise-bundle/part_two.py
@@ -0,0 +1,144 @@
+### 16
+print("\n===== 16 ======\n")
+
+object_seen = "Superman"
+
+if object_seen == "Superman":
+    print("It's a bird! It's a plane! It's Superman!")
+
+### 17
+print("\n===== 17 ======\n")
+
+alingment = "Villain"
+
+if alingment == "Hero":
+     print("Save the cat.")
+else: 
+     print("Destroy the world.")
+
+### 18
+print("\n===== 18 ======\n")
+
+threat_level = 9
+
+if threat_level < 5:
+    print("street level")
+elif threat_level >= 5 and threat_level < 8:
+    print("city level")
+else:
+    print("avengers level")
+
+### 19
+print("\n===== 19 ======\n")
+
+thor = 100
+hulk = 110
+
+if hulk > thor:
+    print("Hulk is stronger")
+
+### 20
+print("\n===== 20 ======\n")
+
+ask_for_password = input("Enter password: ")
+
+if ask_for_password == "Cap":
+    print("Access Granted")
+else:
+    print("Access Denied")
+
+### 21
+print("\n===== 21 ======\n")
+
+is_noble = True
+is_strong = True
+
+if is_noble and is_strong:
+    print("Worthy of Mjolnir")
+
+### 22
+print("\n===== 22 ======\n")
+
+has_shield = False
+has_hammer = True
+
+if has_shield or has_hammer:
+    print("Ready for battle")
+
+### 23
+print("\n===== 23 ======\n")
+
+button_pressed = False
+
+if not button_pressed:
+    print("Safe")
+
+### 24
+print("\n===== 24 ======\n")
+
+ask_for_level = int(input("Insert level: "))
+
+if ask_for_level > 5:
+    what_code = input("Insert Code: ")
+    if what_code == "Alpha":
+        print("\nWelcome Director Fury.")
+    else:
+        print("\nAccess Denied.")
+
+### 25
+print("\n===== 25 ======\n")
+
+team = "Iron Man, Thor, Hulk"
+
+print("Thor" in team)
+
+### 26
+print("\n===== 26 ======\n")
+
+name = ""
+
+if name:
+    print("Name exists.")
+else:
+    print("No name provided.")
+
+### 27
+print("\n===== 27 ======\n")
+
+status = ""
+energy = 50
+
+if energy > 0:
+    status = "Online"
+else:
+    status = "Offline"
+
+print(status)
+
+### 28
+print("\n===== 28 ======\n")
+
+speed = int(input("What's the speed? "))
+
+if speed > 88:    
+    print("Time Travel Imminent.")
+
+### 29
+print("\n===== 29 ======\n")
+
+card = "Ace"
+
+if card != "Joker":
+    print("Safe")
+
+### 30
+print("\n===== 30 ======\n")
+
+missions_completed = 7
+
+if missions_completed > 10:
+    print("Pro")
+elif missions_completed > 5:
+    print("Intermediate") 
+else:
+    print("Rookie")
